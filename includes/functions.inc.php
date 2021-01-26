00f6099aab2ff17b5a38cbad4592c16455e3f7b4
Jose Gonzalez <josegonzalezguilabert@gmail.com>
2021-01-26 19:13:26 +0100

    Login Funcional

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -113,3 +113,46 @@ function createUser($conn, $name, $email, $username, $pwd) {
     exit();
 
 }
+
+
+function emptyInputLogin($username, $pwd) {
+
+    $result;
+
+    if (empty($username) || empty($pwd)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+
+    return $result;
+
+}
+
+function loginUser($conn, $username, $pwd) {
+
+    $uidExists = uidExists($conn, $username, $pwd);
+
+    if ($uidExists === false) {
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $uidExists["userPwd"];
+    $checkPwd = password_verify($pwd, $pwdHashed);
+
+    if ($checkPwd === false) {
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+    else if ($checkPwd === true) {
+        session_start();
+        $_SESSION["userid"] = $uidExists["usersId"];
+        $_SESSION["useruid"] = $uidExists["usersUid"];
+
+        header("location: ../index.php");
+        exit();
+    }
+
+}
--- signup.php
+++ signup.php
@@ -48,8 +48,37 @@
 
 </div>
 
+<?php
+
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput") {
+            echo "<p>Algún campo no ha sido rellenado</p>";
+        }
+        else if ($_GET["error"] == "invaliduid") {
+            echo "<p>El nombre de usuario no es apropiado</p>";
+        }
+        else if ($_GET["error"] == "invalidemail") {
+            echo "<p>El formato de email no es apropiado</p>";
+        }
+        else if ($_GET["error"] == "passwordsdontmatch") {
+            echo "<p>Las contraseñas no coinciden</p>";
+        }
+        else if ($_GET["error"] == "stmtfailed") {
+            echo "<p>Algo ha ido mal. Inténtalo de nuevo</p>";
+        }
+        else if ($_GET["error"] == "usernametaken") {
+            echo "<p>El nombre de usuario ya existe</p>";
+        }
+        else if ($_GET["error"] == "none") {
+            echo "<p>Todo ha salido bien, estás suscrito!!</p>";
+        }
+    }
+
+?>
 
 </section>
+
+
 
 <?php
     include_once 'footer.php';

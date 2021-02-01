93224cf69791ea694a08682201fc064bb97afeac
Jose Gonzalez <josegonzalezguilabert@gmail.com>
2021-02-01 16:56:28 +0100

    Implementado mensage bienvenida

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
     include_once 'header.php';
 ?>
 
+<?php
+
+if (isset($_SESSION["useruid"])) {
+    echo "<p class='bienvenido'>Bienvenido " . $_SESSION["useruid"] . "</p>";
+}
+
+?>
 
 <?php
     include_once 'footer.php';
